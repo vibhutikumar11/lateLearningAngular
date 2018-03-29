3a2a70e51abf081dc34988a7d8b871be0f2ab510
vibhutikumar11@gmail.com <vibhutikumar11@gmail.com>
2018-03-29 09:51:17 +0530

    first routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { MemberComponent } from './member/member.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    MemberComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'member',
+        component: MemberComponent
+      },
+      {
+        path: 'product',
+        component: ProductComponent
+      }
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
