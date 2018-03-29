73568a92b17b3f3ee96d01bf38c26e97781217cd
vibhutikumar11@gmail.com <vibhutikumar11@gmail.com>
2018-03-29 10:46:31 +0530

    some changes in app ts and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /*variable*/
   title = 'Late learning angular !';
+
+  /*object */
+    Obj = {
+      id : 1,
+      name : "vibhuti",
+      email : "vibhutikumar11@gmail.com"
+    }
+
+    /*array*/
+
+    arr = ["vibhuti", "kumar", "jha", "vivek", "Ranjan"];
+
+    /*boolean variable*/
+
+    isTrue = false;
+    myName = "vibhuti";
+
+
 }
